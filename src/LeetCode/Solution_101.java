52ae1173301e99a13ae7f2b8c47fc06823dca615
TenMoons <tenmoons@foxmail.com>
2020-03-16 18:30:32 +0800

    Modify solution 101

--- src/LeetCode/Solution_101.java
+++ src/LeetCode/Solution_101.java
@@ -15,7 +15,9 @@ public class Solution_101 {
     }
 
     public boolean isSymmetric(TreeNode root) {
-        return  isSymmetric(root.left, root.right);
+        if (root == null)
+            return true;
+        return isSymmetric(root.left, root.right);
     }
 
     public boolean isSymmetric(TreeNode p, TreeNode q){
@@ -23,6 +25,11 @@ public class Solution_101 {
             return true;
         if (p == null || q == null)
             return false;
-        return p.val == q.val && isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+        if (p.val == q.val){
+            if (isSymmetric(p.left, q.right)){
+                return isSymmetric(p.right, q.left);
+            }
+        }
+        return false;
     }
 }
