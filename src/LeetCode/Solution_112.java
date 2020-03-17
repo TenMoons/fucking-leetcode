4c07b343f6755a8abe41d7f96bec86f936b826a7
TenMoons <tenmoons@foxmail.com>
2020-03-17 17:08:34 +0800

    Add solution 112

---
+++ src/LeetCode/Solution_112.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+/**
+ * 112.路径总和
+ */
+
+public class Solution_112 {
+    class TreeNode{
+        int val;
+        TreeNode left, right;
+
+        public TreeNode(int x){
+            val = x;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        // 到达根结点
+        if (root.left == null && root.right == null)
+            return sum - root.val == 0;  // 路径总和是否满足sum
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
