f1695fe0bd5d7365d108b3717b5b559b76971ea7
TenMoons <tenmoons@foxmail.com>
2020-03-20 14:36:03 +0800

    Add solution 77

---
+++ src/LeetCode/Solution_77.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+/**
+ * 77.颜色分类
+ * 模板：荷兰国旗问题
+ * 关键思路：快速排序
+ */
+
+public class Solution_77 {
+    public void sortColors(int[] nums) {
+        partition(nums, 0, nums.length - 1, 1);
+    }
+
+    // <k的放左边，==k的放中间，>k的放右边
+    private void partition(int[] arr, int left, int right, int k){
+        int less = left - 1;
+        int more = right + 1;
+        int cur = left;
+        while(cur < more){
+            if(arr[cur] < k){
+                swap(arr, ++less, cur++);
+            }
+            else if(arr[cur] > k){
+                swap(arr, --more, cur);
+            }
+            else{
+                cur++;
+            }
+        }
+    }
+
+    private void swap(int[] arr, int left, int right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+}
