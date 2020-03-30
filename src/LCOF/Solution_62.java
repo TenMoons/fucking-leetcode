5ddce1f858dc29d1cfc1a4d61a7e5a256fd12d90
TenMoons <tenmoons@foxmail.com>
2020-03-30 10:05:01 +0800

    Add solution 面试题62

---
+++ src/LCOF/Solution_62.java
@@ -0,0 +1,30 @@
+package LCOF;
+
+import java.util.ArrayList;
+
+/**
+ * 面试题62. 圆圈中最后剩下的数字
+ */
+
+public class Solution_62 {
+    public int lastRemaining(int n, int m) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            list.add(i);
+        int index = 0;
+        while (n > 0) {
+            index = (index + m - 1) % n;
+            list.remove(index);
+            n--;
+        }
+        return list.get(0);
+    }
+
+    // 解法2：数学
+    public int lastRemaining2(int n, int m) {
+        int res = 0;
+        for (int i = 2; i <= n; i++)
+            res = (res + m) % i;
+        return res;
+    }
+}
