53c2755fefd6b27cff8db158dde6c46f6e8cfac8
TenMoons <tenmoons@foxmail.com>
2020-04-25 21:46:55 +0800

    Add solutions

---
+++ src/Algorithms/Solution_1248.java
@@ -0,0 +1,27 @@
+package Algorithms;
+
+/**
+ * 1248. 统计「优美子数组」
+ */
+
+public class Solution_1248 {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int res = 0;
+        int oddCount = 0;
+        int[] arr = new int[nums.length + 2];
+        // 记录奇数的下标
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] & 1) == 1) {
+                arr[++oddCount] = i;//第++oddCount个奇数的下标是i
+            }
+        }
+        arr[0] = -1; //左边界
+        arr[oddCount + 1] = nums.length; //右边界
+
+        // 相减的结果为两个相邻奇数之间的偶数个数，可随意添加！
+        for (int i = 1; i + k < oddCount + 2; i++) {
+            res += (arr[i] - arr[i - 1]) * (arr[i + k] - arr[i + k - 1]);
+        }
+        return res;
+    }
+}
---
+++ src/Algorithms/Solution_338.java
@@ -0,0 +1,47 @@
+package Algorithms;
+
+/**
+ * 338. 比特位计数
+ */
+
+public class Solution_338 {
+    // O(n * len(Integer))
+    public int[] countBits(int num) {
+        int[] res = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            res[i] = countOne(i);
+        }
+        return res;
+    }
+
+    private int countOne(int val) {
+        int cnt = 0;
+        while (val != 0) {
+            cnt += val & 1;
+            val >>= 1;
+        }
+        return cnt;
+    }
+
+    // O(n): 动态规划
+    public int[] countBits2(int num) {
+        int[] dp = new int[num + 1];
+        dp[0] = 0;
+        if (num == 0)
+            return dp;
+        dp[1] = 1;
+        int left = 1, right = 2;
+        for (int i = 2; i <= num; i++) {
+            // i是2的幂
+            if (right == i) {
+                // 更新上下界
+                left *= 2;
+                right *= 2;
+                dp[i] = 1;
+            } else {
+                dp[i] = dp[left] + dp[i - left];
+            }
+        }
+        return dp;
+    }
+}
