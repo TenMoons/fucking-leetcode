6fd5ddafaa152b6a6a39c7576e13c81ba952752b
TenMoons <tenmoons@foxmail.com>
2020-04-29 23:20:20 +0800

    Add solutions 34/81/153/154/1095

--- src/Algorithms/Solution_34.java
+++ src/Algorithms/Solution_34.java
@@ -5,27 +5,25 @@ public class Solution_34 {
     int end = Integer.MIN_VALUE;
 
     public int[] searchRange(int[] nums, int target) {
-        digui(nums, target, 0, nums.length-1);
-        if(begin == Integer.MAX_VALUE || end == Integer.MIN_VALUE) return new int[]{-1,-1};
+        binarySearch(nums, target, 0, nums.length-1);
+        if (begin == Integer.MAX_VALUE || end == Integer.MIN_VALUE)
+            return new int[]{-1,-1};
         return new int[]{begin, end};
     }
 
-    public void digui(int[] nums, int target ,int l, int r){
-        if(l > r) return;
-        int mid = l + (r-l)/2;
-        if(target == nums[mid]){
-            processResult(mid);
-            digui(nums, target, mid + 1, r);
-            digui(nums, target, l, mid -1);
+    public void binarySearch(int[] nums, int target ,int l, int r){
+        if(l > r)
+            return;
+        int mid = l + (r - l) / 2;
+        if (target == nums[mid]) {
+            begin = Math.min(begin, mid);
+            end = Math.max(end, mid);
+            binarySearch(nums, target, mid + 1, r);
+            binarySearch(nums, target, l, mid - 1);
         } else if (target > nums[mid]) {
-            digui(nums, target, mid + 1, r);
+            binarySearch(nums, target, mid + 1, r);
         } else {
-            digui(nums, target, l, mid -1);
+            binarySearch(nums, target, l, mid - 1);
         }
-    }
-
-    public void processResult(int pos){
-        if(pos < begin) begin = pos;
-        if(pos > end) end = pos;
     }
 }
