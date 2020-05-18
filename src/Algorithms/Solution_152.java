46ec8fcdad147a4a98b0c64ffed6d2acfacf7439
TenMoons <tenmoons@foxmail.com>
2020-05-18 20:39:31 +0800

    Add solutions 152/207/210

---
+++ src/Algorithms/Solution_152.java
@@ -0,0 +1,53 @@
+package Algorithms;
+
+/**
+ * 152. 乘积最大子数组
+ */
+
+public class Solution_152 {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        // dp[i][0]:以nums[i]结尾的连续子数组的乘积最小值
+        // dp[i][1]:以nums[i]结尾的连续子数组的乘积最大值
+        int[][] dp = new int[nums.length][2];
+        dp[0][0] = dp[0][1] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                dp[i][1] = Math.max(nums[i], nums[i] * dp[i - 1][1]);
+                dp[i][0] = Math.min(nums[i], nums[i] * dp[i - 1][0]);
+            } else {
+                dp[i][1] = Math.max(nums[i], nums[i] * dp[i - 1][0]);  // 因为nums[i]为负数，若dp[i-1][0]是负数，则负负得正，若是正数，则得到更小值
+                dp[i][0] = Math.min(nums[i], nums[i] * dp[i - 1][1]);
+            }
+        }
+        int res = dp[0][1];
+        for (int[] d : dp) {
+            res = Math.max(res, d[1]);
+        }
+        return res;
+    }
+
+    // 优化
+    public int maxProduct2(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int res = nums[0];
+        int preMax = nums[0];
+        int preMin = nums[0];
+        int curMax, curMin;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                curMax = Math.max(nums[i], nums[i] * preMax);
+                curMin = Math.min(nums[i], nums[i] * preMin);
+            } else {
+                curMax = Math.max(nums[i], nums[i] * preMin);
+                curMin = Math.min(nums[i], nums[i] * preMax);
+            }
+            res = Math.max(res, curMax);
+            preMax = curMax;
+            preMin = curMin;
+        }
+        return res;
+    }
+}
---
+++ src/Algorithms/Solution_207.java
@@ -0,0 +1,46 @@
+package Algorithms;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 207. 课程表
+ */
+
+public class Solution_207 {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses == 1) {
+            return true;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        int[] inDegree = new int[numCourses];  // 记录每个顶点的入度
+        List<List<Integer>> adj = new ArrayList<>();  // 图的邻接表
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] prerequisite : prerequisites) {
+            inDegree[prerequisite[0]]++;  // 初始化入度数组
+            adj.get(prerequisite[1]).add(prerequisite[0]);  // 构建邻接表
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);  // 入度为0的点入队
+            }
+        }
+        int cnt = 0;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            cnt++;
+            for (int i = 0; i < adj.get(cur).size(); i++) {
+                int v = adj.get(cur).get(i);
+                inDegree[v]--;
+                if (inDegree[v] == 0) {
+                    queue.add(v);
+                }
+            }
+        }
+        return cnt == numCourses;
+    }
+}
