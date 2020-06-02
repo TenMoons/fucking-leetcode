2c466cef4dc7e6a0e971f517ed7fd94eb152a121
TenMoons <tenmoons@foxmail.com>
2020-06-02 14:06:49 +0800

    Add solutions

---
+++ src/interview/interview7/Solution_3.java
@@ -0,0 +1,25 @@
+package interview.interview7;
+
+/**
+ * 翻转二进制矩阵图像
+ */
+
+public class Solution_3 {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for (int[] arr : A) {
+            reverse(arr, 0, arr.length - 1);
+        }
+        return A;
+    }
+
+    private void reverse(int[] arr, int lo, int hi) {
+        while (lo < hi) {
+            int temp = (arr[lo] ^ 1);
+            arr[lo++] = (arr[hi] ^ 1);
+            arr[hi--] = temp;
+        }
+        if (lo == hi) {
+            arr[lo] ^= 1;
+        }
+    }
+}
