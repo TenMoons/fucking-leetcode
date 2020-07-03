58ebb475c568e8b00b1aef26028ed530b3f832c0
TenMoons <tenmoons@foxmail.com>
2020-07-03 20:41:58 +0800

    Add solutions

---
+++ src/Algorithms/Solution_1035.java
@@ -0,0 +1,25 @@
+package Algorithms;
+
+/**
+ * 1035. 不相交的线
+ */
+
+public class Solution_1035 {
+    public int maxUncrossedLines(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int[][] dp = new int[m + 1][n + 1];
+        int res = 0;
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+                res = Math.max(res, dp[i][j]);
+            }
+        }
+        return res;
+    }
+}
---
+++ src/Algorithms/Solution_718.java
@@ -0,0 +1,22 @@
+package Algorithms;
+
+/**
+ * 718. 最长重复子数组
+ */
+
+public class Solution_718 {
+    public int findLength(int[] A, int[] B) {
+        // dp[i][j]表示以A[i],B[j]结尾的子数组中公共部分的最大长度
+        int[][] dp = new int[A.length + 1][B.length + 1];
+        int res = 0;
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 1; j <= B.length; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    res = Math.max(res, dp[i][j]);
+                }
+            }
+        }
+        return res;
+    }
+}
